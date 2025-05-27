df8be48e2505fde4627c91c4092ea35bdb831af1
Kiran Veerabatheni <kiran.veerabatheni@hotmail.com>
2025-05-26 23:59:10 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SecurityAnalysisApp from './components/SecurityAnalysisApp';
+
+function App() {
+  return (
+    <div className="App">
+      <SecurityAnalysisApp />
+    </div>
+  );
+}
+
+export default App;
